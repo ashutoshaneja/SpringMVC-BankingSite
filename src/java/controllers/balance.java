2f6d47d1e43e2e0186b1798554527cc48229d0ce
Ashutosh Aneja <aaneja0007@gmail.com>
2019-07-18 23:58:45 +0530

    Update balance.java

--- src/java/controllers/balance.java
+++ src/java/controllers/balance.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/balance")
+@RequestMapping (value = "/balance")
 public class balance {
     Connection cn;
 String url,sql;
